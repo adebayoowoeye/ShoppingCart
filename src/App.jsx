7f140dd09e9117de14a13b2f935e769df134a0b2
Adebayo Owoeye <owoeye0075@gmail.com>
2024-07-20 11:03:13 +0100

    Add project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-blue-600 text-4xl">Shopping Cart in progress</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
