cc07a5c56f8be32132ec7ac2452b664317c0f627
Adebayo Owoeye <owoeye0075@gmail.com>
2024-07-27 12:33:35 +0100

    Implement the Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1 className="text-blue-600 text-4xl mt-28">
         Shopping Cart in progress
       </h1>
+      <Footer />
     </main>
   );
 }
